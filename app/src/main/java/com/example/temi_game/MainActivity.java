1ad6d4aca5a7b3d108242253e8577a2acd2f0a32
ahmed-ghaleb-2022 <ahmedtareq19932022@gmail.com>
2023-07-04 14:22:56 +0200

    upload the project

---
+++ app/src/main/java/com/example/temi_game/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.temi_game;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    public void StartMainMenu(View view) {
+        Intent intent = new Intent(MainActivity.this, MainMenu.class);
+        startActivity(intent);
+    }
+}
